c0cf4f601925959c813262044955fc7484273507
JTDoe <Jordendoerflinger@gmail.com>
2022-06-09 16:22:59 -0500

    first commit

--- index.js
+++ index.js
@@ -2,11 +2,26 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
-
+const newUser = {
+"_id": 6,
+"name": "Don Pooper",
+"occupation": "TSA Agent",
+"avatar": "https://upload.wikimedia.org/wikipedia/en/5/50/AgentDonePooper.jpg"}
 const { users } = require('./state')
 
 /* BEGIN - create routes here */
-
+app.get('/users', (req, res) => {
+return res.json(users)
+})
+app.get('/users/1', (req, res) => {
+  return res.json(users[0])
+})
+app.post('/users', (req, res) => {
+  return res.json(users.push(newUser))
+})
+app.put('/users/1', (req, res) => {
+  return res.json(users)
+})
 
 /* END - create routes here */
 
